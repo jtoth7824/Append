d5047e128a9580eb0e96af8290f6fd389a4fd9e1
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-08-11 20:52:42 -0400

    removed unnecessary read of file

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const filename = process.argv[2];
 // text string to append to the file with a new line appended prior to the text
 const appendText = "\nHello World";
 
-// Return the contents of 'data.csv' as a string in the variable "data"
-// "utf8" encodes the raw buffer data in human-readable format
-fs.readFile(filename, 'utf8', (error, data) =>
-  error ? console.error(error) : console.log(data)
-);
-
 // utilize Node standard library file system package for appending text to file
 fs.appendFile(filename, appendText, (err) =>
   // check if error condition and log it
